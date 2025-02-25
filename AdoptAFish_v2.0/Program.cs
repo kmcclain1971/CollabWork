e1e1042d8ba21fcd81b7ae5aabeacea72b3b5d2a
UCMED\jjx8347 <kevin.mcclain@uchicagomedicine.org>
2025-02-25 15:27:50 -0600

    - Finished setting intial console look and feel

--- AdoptAFish_v2.0/Program.cs
+++ AdoptAFish_v2.0/Program.cs
@@ -4,6 +4,15 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SetConsoleProperties();
+        //TODO: Start application from here
+    }
+
+    private static void SetConsoleProperties()
+    {
+        Console.Title = "Adopt A Fish App";
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+        Console.Clear();
     }
 }
