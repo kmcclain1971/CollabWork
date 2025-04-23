ee67938c3d83f4a44eb8b67e31bb9cabf562cbb9
Conor McClain <Conor McClain@DESKTOP-5JOKC0A>
2025-04-22 20:40:20 -0500

    - created opening screen - created location choice screen

--- WaywardHorizonsWPF/WaywardHorizons/Locations.xaml.cs
+++ WaywardHorizonsWPF/WaywardHorizons/Locations.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,23 +16,13 @@ using System.Windows.Shapes;
 namespace WaywardHorizons
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Locations.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Locations : Page
     {
-        public MainWindow()
+        public Locations()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
