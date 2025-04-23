1a879366045c936c621ad657aae3d3a6d29c11c4
Conor McClain <Conor McClain@DESKTOP-5JOKC0A>
2025-04-23 13:27:35 -0500

    -created menu page

--- WaywardHorizonsWPF/WaywardHorizons/MainWindow.xaml.cs
+++ WaywardHorizonsWPF/WaywardHorizons/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace WaywardHorizons
 {
@@ -16,6 +18,8 @@ namespace WaywardHorizons
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public Player ThePlayer = new Player();
         public MainWindow()
         {
             InitializeComponent();
@@ -23,7 +27,7 @@ namespace WaywardHorizons
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
---
+++ WaywardHorizonsWPF/WaywardHorizons/Menu.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WaywardHorizons
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        MainWindow window = (MainWindow)Application.Current.MainWindow;
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                //process the input
+                ProcessPlayerInput();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessPlayerInput();
+        }
+
+        private void ProcessPlayerInput()
+        {
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                window.ThePlayer.PlayerName = "Player";
+            }
+            else
+            {
+                //playerinput
+                window.ThePlayer.PlayerName = txtName.Text;
+            }
+
+            //swap pages from menu to location
+            //template:
+            //NavigationService.Navigate(new Uri("", UriKind.Relative));
+            NavigationService.Navigate(new Uri("Locations.xaml", UriKind.Relative));
+        }
+    }
+}
